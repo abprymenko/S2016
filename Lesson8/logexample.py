4712eeb6053bb6ab604b4dac2877b363d142af88
AndrewPrymenko <ffeelex@gmail.com>
2023-06-03 19:56:58 +0200

    #lesson 8 - changes

--- Lesson8/logexample.py
+++ Lesson8/logexample.py
@@ -1,36 +1,28 @@
-from logging import debug, info, warning, error, critical, basicConfig, \
-    DEBUG, ERROR, WARNING, INFO, CRITICAL
+from logging import debug, info, warning, error, critical, basicConfig
 class Logging:
     def __init__(self, message = ""):
         self.Message = message
-    def __str__(self):
-        if(self.Message == "" or self.Message == None):
-            raise TypeError("Value cann't be None or empty")
-        return self.Message
-    def LogConfig(self, loginLevel = DEBUG):
-                fileName = "filelog.log"
-                level = DEBUG
-                format = "%(asctime)s : %(levelname)s - %(message)s"
-                match loginLevel:
-                    case 10:
-                        level = DEBUG
-                        basicConfig(level=level, format=format, filename=fileName, filemode='a')
-                        debug(self.__str__())
-                    case 20:
-                        level = INFO
-                        basicConfig(level=level, format=format, filename=fileName, filemode='a')
-                        info(self.__str__())
-                    case 30:
-                        level = WARNING
-                        basicConfig(level=level, format=format, filename=fileName, filemode='a')
-                        warning(self.__str__())
-                    case 40:
-                        level = ERROR
-                        basicConfig(level=level, format=format, filename=fileName, filemode='a')
-                        error(self.__str__())
-                    case 50:
-                        level = CRITICAL
-                        basicConfig(level=level, format=format, filename=fileName, filemode='a')
-                        critical(self.__str__())
-    def Log(self, loginLevel):
-        self.LogConfig(loginLevel)
+    def LogConfig(self):
+        def Wrapper(*args, **kwargs):
+            fileName = "filelog.log"
+            self.Message = args[2]
+            if (self.Message == "" or self.Message == None):
+                raise TypeError("Value cann't be None or empty")
+            format = "%(asctime)s : %(levelname)s - %(message)s"
+            basicConfig(level=args[1], format=format, filename=fileName, filemode='a')
+            match args[1]:
+                case 10:
+                    debug(self.Message)
+                case 20:
+                    info(self.Message)
+                case 30:
+                    warning(self.Message)
+                case 40:
+                    error(self.Message)
+                case 50:
+                    critical(self.Message)
+        return Wrapper
+    @LogConfig
+    def Log(self, loglevel, message):
+        self.Message = message
+        return self
--- Lesson8/main.py
+++ Lesson8/main.py
@@ -4,21 +4,16 @@ while(True):
     try:
         digit1 = int(input("Enter digit1: "))
         digit2 = int(input("Enter digit2: "))
-        log.Message = f"{digit1};\t{digit2}"
-        log.Log(20)
+        log.Log(20, f"{digit1};\t{digit2}")
         print(digit1/digit2)
     except ZeroDivisionError as zde:
-        log.Message = zde
-        log.Log(40)
+        log.Log(40, zde)
     except TypeError as te:
-        log.Message = te
-        log.Log(40)
+        log.Log(40, te)
     except Exception as ex:
-        log.Message = ex
-        log.Log(40)
+        log.Log(40, ex)
     finally:
         yes = input("Do you want to try again?[y/n]: ")
         if(yes.lower() != 'y'):
-            log.Message = "End!"
-            log.Log(20)
+            log.Log(20, "End!")
             break
